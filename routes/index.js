f37bb6bf318cdf55a7dc06cab35e7717637d2e0e
Soulside44 <soulside44@gmail.com>
2019-12-10 11:00:04 +0900

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('홍길동', { title: '홍길동' });
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
